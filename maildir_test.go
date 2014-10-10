65ea2b33b78d3e3f642ce399dc0c4491da321f38
Lukas Senger <lukas@fridolin.com>
2014-10-11 00:04:17 +0200

    Add tests

---
+++ maildir_test.go
@@ -0,0 +1,169 @@
+package maildir
+
+import (
+	"io/ioutil"
+	"os"
+	"testing"
+)
+
+// cleanup removes a Dir's directory structure
+func cleanup(t *testing.T, d Dir) {
+	err := os.RemoveAll(string(d))
+	if err != nil {
+		t.Error(err)
+	}
+}
+
+// exists checks if the given path exists
+func exists(path string) bool {
+	_, err := os.Stat(path)
+	if err == nil {
+		return true
+	}
+	if os.IsNotExist(err) {
+		return false
+	}
+	panic(err)
+}
+
+// cat returns the content of a file as a string
+func cat(t *testing.T, path string) string {
+	f, err := os.Open(path)
+	if err != nil {
+		t.Fatal(err)
+	}
+	defer f.Close()
+	c, err := ioutil.ReadAll(f)
+	if err != nil {
+		t.Fatal(err)
+	}
+	return string(c)
+}
+
+// makeDelivery creates a new message
+func makeDelivery(t *testing.T, d Dir, msg string) {
+	del, err := d.NewDelivery()
+	if err != nil {
+		t.Fatal(err)
+	}
+	_, err = del.Write([]byte(msg))
+	if err != nil {
+		t.Fatal(err)
+	}
+	err = del.Close()
+	if err != nil {
+		t.Fatal(err)
+	}
+}
+
+func TestCreate(t *testing.T) {
+	t.Parallel()
+
+	var d Dir = "test_create"
+	err := d.Create()
+	if err != nil {
+		t.Fatal(err)
+	}
+	defer cleanup(t, d)
+}
+
+func TestDelivery(t *testing.T) {
+	t.Parallel()
+
+	var d Dir = "test_delivery"
+	err := d.Create()
+	if err != nil {
+		t.Fatal(err)
+	}
+	defer cleanup(t, d)
+
+	var msg = "this is a message"
+	makeDelivery(t, d, msg)
+
+	keys, err := d.Unseen()
+	if err != nil {
+		t.Fatal(err)
+	}
+	path, err := d.Filename(keys[0])
+	if err != nil {
+		t.Fatal(err)
+	}
+	if !exists(path) {
+		t.Fatal("File doesn't exist")
+	}
+
+	if cat(t, path) != msg {
+		t.Fatal("Content doesn't match")
+	}
+}
+
+func TestPurge(t *testing.T) {
+	t.Parallel()
+
+	var d Dir = "test_purge"
+	err := d.Create()
+	if err != nil {
+		t.Fatal(err)
+	}
+	defer cleanup(t, d)
+
+	makeDelivery(t, d, "foo")
+
+	keys, err := d.Unseen()
+	if err != nil {
+		t.Fatal(err)
+	}
+	path, err := d.Filename(keys[0])
+	if err != nil {
+		t.Fatal(err)
+	}
+	err = d.Purge(keys[0])
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	if exists(path) {
+		t.Fatal("File still exists")
+	}
+}
+
+func TestMove(t *testing.T) {
+	t.Parallel()
+
+	var d1 Dir = "test_move1"
+	err := d1.Create()
+	if err != nil {
+		t.Fatal(err)
+	}
+	defer cleanup(t, d1)
+	var d2 Dir = "test_move2"
+	err = d2.Create()
+	if err != nil {
+		t.Fatal(err)
+	}
+	defer cleanup(t, d2)
+
+	const msg = "a moving message"
+	makeDelivery(t, d1, msg)
+	keys, err := d1.Unseen()
+	if err != nil {
+		t.Fatal(err)
+	}
+	err = d1.Move(d2, keys[0])
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	keys, err = d2.Keys()
+	if err != nil {
+		t.Fatal(err)
+	}
+	path, err := d2.Filename(keys[0])
+	if err != nil {
+		t.Fatal(err)
+	}
+	if cat(t, path) != msg {
+		t.Fatal("Content doesn't match")
+	}
+
+}
