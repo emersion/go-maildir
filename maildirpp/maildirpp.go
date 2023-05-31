6499f1df8af4b15dd3300ba1dd018f1341a0a0e8
Vassili Tchersky <vt+git@vbc.su>
2023-05-31 15:26:03 +0200

    Add maildirpp.Walk

--- maildirpp/maildirpp.go
+++ maildirpp/maildirpp.go
@@ -3,7 +3,11 @@ package maildirpp
 
 import (
 	"errors"
+	"os"
+	"path/filepath"
 	"strings"
+
+	"github.com/emersion/go-maildir"
 )
 
 const separator = '.'
@@ -22,4 +26,34 @@ func Join(elems []string) (key string, err error) {
 		}
 	}
 	return "." + strings.Join(elems, string(separator)), nil
+}
+
+func Dir(root, key string) maildir.Dir {
+	return maildir.Dir(filepath.Join(root, key))
+}
+
+// Walk calls fn for every Maildir++ subfolders of the root directory.
+//
+// It stops if fn returns a non-nil error.
+func Walk(root string, fn func(key string) error) error {
+	f, err := os.Open(root)
+	if err != nil {
+		return err
+	}
+
+	dis, err := f.ReadDir(0)
+	f.Close()
+	if err != nil {
+		return err
+	}
+
+	for _, di := range dis {
+		if di.IsDir() && di.Name()[0] == separator {
+			if err := fn(di.Name()); err != nil {
+				return err
+			}
+		}
+	}
+
+	return nil
 }
