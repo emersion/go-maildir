9815f5435a34cecd1d6eb510562baa207c3a6867
Kai O'Reilly <koreilly5297@gmail.com>
2023-11-23 11:43:19 -0800

    use a semicolon for the separator on windows

---
+++ maildir_notwindows.go
@@ -0,0 +1,8 @@
+//go:build !windows
+
+package maildir
+
+// The separator separates a messages unique key from its flags in the filename.
+// This should only be changed on operating systems where the colon isn't
+// allowed in filenames.
+const separator rune = ':'
