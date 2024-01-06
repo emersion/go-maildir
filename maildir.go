emersion/go-maildir

commit 2360c2f48305f1ec14bc83be5af2923b8ef30546

Add workaround for offlineimap
Offlineimap appends an info section to messages in `new` and `Unseen()`
used to add another info section after that. The correct behaviour is to
have no info section in `new`

maildir.go
@@ -71,9 +71,17 @@ func (d Dir) Unseen() ([]string, error) {
 			split := strings.FieldsFunc(n, func(r rune) bool {
 				return r == Separator
 			})
-			keys = append(keys, split[0])
+			key := split[0]
+			info := "2,"
+			// Messages in new shouldn't have an info section but
+			// we act as if, in case some other program didn't
+			// follow the spec.
+			if len(split) > 1 {
+				info = split[1]
+			}
+			keys = append(keys, key)
 			err = os.Rename(filepath.Join(string(d), "new", n),
-				filepath.Join(string(d), "cur", n+string(Separator)+"2,"))
+				filepath.Join(string(d), "cur", key+string(Separator)+info))
 		}
 	}
 	return keys, err

commit 4bb7e93c82d4cec06ba94772f30ed2a118074a55

Discard info field for messages in new/
Programs writing to new/ shouldn't have control over flags.

Fixes: 2360c2f48305 ("Add workaround for offlineimap")

maildir.go
@@ -88,17 +88,14 @@ func (d Dir) Unseen() ([]string, error) {
 				continue
 			}
 
+			// Messages in new shouldn't have an info field, but some programs
+			// (e.g. offlineimap) do that anyways. Discard the info field in
+			// that case.
 			split := strings.FieldsFunc(n, func(r rune) bool {
 				return r == separator
 			})
 			key := split[0]
 			info := "2,"
-			// Messages in new shouldn't have an info section but
-			// we act as if, in case some other program didn't
-			// follow the spec.
-			if len(split) > 1 {
-				info = split[1]
-			}
 			keys = append(keys, key)
 
 			err := os.Rename(filepath.Join(string(d), "new", n),